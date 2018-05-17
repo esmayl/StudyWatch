5ac42603757f01f537037fb1075cea2ce29e7d13
esmayl <esmayl@jellyfishdm.com>
2018-05-17 13:31:11 +0200

    Creation:
    * Login now looks like theme "https://codepen.io/flannerydesigns/pen/dPYzNR"

---
+++ StudyWatch/views/login.php
@@ -0,0 +1,24 @@
+<div class="vid-container">
+	<img class="bgvid" <source src="https://images.unsplash.com/photo-1425321395722-b1dd54a97cf3?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="> />
+	
+	<div class="inner-container">
+    
+		<img class="bgvid inner" src="https://images.unsplash.com/photo-1425321395722-b1dd54a97cf3?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+	
+		<div class="box">
+		
+		  <h1>Login</h1>
+		  
+		  <input type="text" placeholder="Username"/>
+		  
+		  <input type="text" placeholder="Password"/>
+		  
+		  <button>Login</button>
+		  
+		  <p>Not a member? <span>Sign Up</span></p>
+		  
+		</div>
+		
+	</div>
+	
+</div>
