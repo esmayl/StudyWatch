2b279c6d7250463a76a712adfa350c91dc1c10aa
Demy <32939076+DemyDan@users.noreply.github.com>
2018-06-02 23:07:43 +0200

    Studenten pagina is cleaner

--- StudyWatch/views/home.php
+++ StudyWatch/views/home.php
@@ -71,21 +71,6 @@
   </aside>
   
   <div class="content-wrapper">
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Welkom</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a name="home" href="#">Home</a></li>
-              <li class="breadcrumb-item active">Projectmanagement</li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </section>
           <div class="card">
             <div class="card-header">
               <h3 class="card-title">Lessen</h3>
