3e3aef55afd6291f7893b7307f386003dad9a54d
DemyDan <demydanielsson@gmail.com>
2018-06-03 16:25:09 +0200

    Database voor de markt erin gezet
    We zitten nu bij week 4 volgens dit

--- database/studywatch.sql
+++ database/studywatch.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 03 jun 2018 om 12:48
+-- Gegenereerd op: 03 jun 2018 om 16:24
 -- Serverversie: 10.1.26-MariaDB
 -- PHP-versie: 7.1.9
 
@@ -41,29 +41,78 @@ CREATE TABLE `attendency` (
 --
 
 INSERT INTO `attendency` (`ID`, `student_id`, `subject_id`, `class_id`, `attendance`) VALUES
-(1, 1, 1, 1, 'Afwezig'),
-(2, 1, 1, 2, 'Afwezig'),
-(3, 1, 1, 4, 'Afwezig'),
-(7, 1, 2, 1, 'Afwezig'),
-(8, 1, 2, 2, 'Afwezig'),
-(9, 1, 2, 3, 'Afwezig'),
-(10, 1, 2, 4, 'Afwezig'),
-(11, 1, 3, 1, 'Afwezig'),
-(12, 1, 3, 2, 'Afwezig'),
-(13, 1, 3, 3, 'Afwezig'),
-(14, 1, 3, 4, 'Afwezig'),
-(15, 1, 4, 1, 'Afwezig'),
-(16, 1, 4, 2, 'Afwezig'),
-(17, 1, 4, 3, 'Afwezig'),
-(18, 1, 4, 4, 'Afwezig'),
-(19, 1, 5, 1, 'Afwezig'),
-(20, 1, 5, 2, 'Afwezig'),
-(21, 1, 5, 3, 'Afwezig'),
-(22, 1, 5, 4, 'Afwezig'),
-(23, 1, 6, 1, 'Afwezig'),
-(24, 1, 6, 2, 'Afwezig'),
-(25, 1, 6, 3, 'Afwezig'),
-(26, 1, 6, 4, 'Afwezig');
+(35, 1, 1, 1, 'Aanwezig'),
+(36, 1, 1, 2, 'Aanwezig'),
+(37, 1, 1, 3, 'Aanwezig'),
+(38, 1, 1, 4, 'Afwezig'),
+(39, 1, 2, 1, 'Aanwezig'),
+(40, 1, 2, 2, 'Aanwezig'),
+(41, 1, 2, 3, 'Aanwezig'),
+(42, 1, 2, 4, 'Afwezig'),
+(43, 1, 3, 1, 'Aanwezig'),
+(44, 1, 3, 2, 'Aanwezig'),
+(45, 1, 3, 3, 'Aanwezig'),
+(46, 1, 3, 4, 'Afwezig'),
+(47, 1, 4, 1, 'Aanwezig'),
+(48, 1, 4, 2, 'Aanwezig'),
+(49, 1, 4, 3, 'Aanwezig'),
+(50, 1, 4, 4, 'Afwezig'),
+(51, 1, 5, 1, 'Aanwezig'),
+(52, 1, 5, 2, 'Aanwezig'),
+(53, 1, 5, 3, 'Aanwezig'),
+(54, 1, 5, 4, 'Afwezig'),
+(55, 1, 6, 1, 'Aanwezig'),
+(56, 1, 6, 2, 'Aanwezig'),
+(57, 1, 6, 3, 'Aanwezig'),
+(58, 1, 6, 4, 'Afwezig'),
+(59, 2, 1, 1, 'Afwezig'),
+(60, 2, 1, 2, 'Aanwezig'),
+(61, 2, 1, 3, 'Aanwezig'),
+(62, 2, 1, 4, 'Afwezig'),
+(63, 2, 2, 1, 'Afwezig'),
+(64, 2, 2, 2, 'Afwezig'),
+(65, 2, 2, 3, 'Aanwezig'),
+(66, 2, 2, 4, 'Afwezig'),
+(67, 2, 3, 1, 'Aanwezig'),
+(68, 2, 3, 2, 'Afwezig'),
+(69, 2, 3, 3, 'Aanwezig'),
+(70, 2, 3, 4, 'Afwezig'),
+(71, 2, 4, 1, 'Afwezig'),
+(72, 2, 4, 2, 'Aanwezig'),
+(73, 2, 4, 3, 'Aanwezig'),
+(74, 2, 4, 4, 'Afwezig'),
+(75, 2, 5, 1, 'Afwezig'),
+(76, 2, 5, 2, 'Afwezig'),
+(77, 2, 5, 3, 'Afwezig'),
+(78, 2, 5, 4, 'Afwezig'),
+(79, 2, 6, 1, 'Aanwezig'),
+(80, 2, 6, 2, 'Afwezig'),
+(81, 2, 6, 3, 'Aanwezig'),
+(82, 2, 6, 4, 'Afwezig'),
+(83, 3, 1, 1, 'Afwezig'),
+(84, 3, 1, 2, 'Afwezig'),
+(85, 3, 1, 3, 'Aanwezig'),
+(86, 3, 1, 4, 'Afwezig'),
+(87, 3, 2, 1, 'Afwezig'),
+(88, 3, 2, 2, 'Afwezig'),
+(89, 3, 2, 3, 'Aanwezig'),
+(90, 3, 2, 4, 'Afwezig'),
+(91, 3, 3, 1, 'Afwezig'),
+(92, 3, 3, 2, 'Afwezig'),
+(93, 3, 3, 3, 'Aanwezig'),
+(94, 3, 3, 4, 'Afwezig'),
+(95, 3, 4, 1, 'Afwezig'),
+(96, 3, 4, 2, 'Aanwezig'),
+(97, 3, 4, 3, 'Aanwezig'),
+(98, 3, 4, 4, 'Afwezig'),
+(99, 3, 5, 1, 'Afwezig'),
+(100, 3, 5, 2, 'Afwezig'),
+(101, 3, 5, 3, 'Afwezig'),
+(102, 3, 5, 4, 'Afwezig'),
+(103, 3, 6, 1, 'Aanwezig'),
+(104, 3, 6, 2, 'Afwezig'),
+(105, 3, 6, 3, 'Afwezig'),
+(106, 3, 6, 4, 'Afwezig');
 
 -- --------------------------------------------------------
 
@@ -73,21 +122,21 @@ INSERT INTO `attendency` (`ID`, `student_id`, `subject_id`, `class_id`, `attenda
 
 CREATE TABLE `class` (
   `id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL
+  `subject_id` int(11) NOT NULL,
+  `classesPast` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `class`
 --
 
-INSERT INTO `class` (`id`, `subject_id`) VALUES
-(2, 1),
-(1, 2),
-(7, 2),
-(4, 3),
-(3, 4),
-(6, 5),
-(5, 6);
+INSERT INTO `class` (`id`, `subject_id`, `classesPast`) VALUES
+(1, 2, 4),
+(2, 1, 4),
+(3, 4, 4),
+(4, 3, 4),
+(5, 6, 4),
+(6, 5, 4);
 
 -- --------------------------------------------------------
 
@@ -205,13 +254,13 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT voor een tabel `attendency`
 --
 ALTER TABLE `attendency`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT voor een tabel `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `students`
