52c7ba6f5f1bfd33bea11a1d0cbcbd4197fd59c1
esmayl <esmayl@jellyfishdm.com>
2018-06-03 15:49:34 +0200

    Edits:
    * Organized project
    * Minor style tweaks

--- htdocs/StudyWatch/index.php
+++ htdocs/StudyWatch/index.php
@@ -3,6 +3,6 @@
 // the location where all files are stored
 define('APP_PATH',  'C:\Users\esmayl\Dropbox\Windesheim\Periode 4\P004-StudyWatch\Repo\StudyWatch\StudyWatch');
 
-require_once(APP_PATH . '/controllers/db_conection.php');
+require_once(APP_PATH . '/models/db_conection.php');
 require_once(APP_PATH . '/user.php');
 require_once(APP_PATH . '/routes.php');
